78217cb7406d5ffb33567973a062cded9b9e9a28
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-13 15:19:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Menu from "./Menu";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Restaurant menu app</h1>
+      <Menu />
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    id: 1,
+    category: "Breakfast 1",
+    desc: "ghjgjgjgjgjugjgujgsjg",
+    price: 16,
+    img: ""
+  },
+  {
+    id: 2,
+    category: "Lunch 1",
+    desc: "ghjgjgjgjgjugjgujgsjg ü+p+ooli678tuzghlmljlk",
+    price: 18,
+    img: ""
+  },
+
+  {
+    id: 3,
+    category: "Dinner 1",
+    desc: "ghjgjgjgjgjugjgujgsjg y<nygvgcgvbm,vnmc.mblc",
+    price: 19,
+    img: ""
+  },
+
+  {
+    id: 4,
+    category: "Breakfast 2",
+    desc: "ghjgjgjgjgjugjgujgsjg huhzuizhihbbjmüpüpopwqwe",
+    price: 17,
+    img: ""
+  },
+
+  {
+    id: 5,
+    category: "Lunch 2",
+    desc: "ghjgjgjgjgjugjgujgsjg",
+    price: 16,
+    img: ""
+  },
+  {
+    id: 6,
+    category: "Dinner 2",
+    desc: "ghjgjgjgjgjugjgujgsjg dvdxvdxv",
+    price: 10,
+    img: ""
+  },
+
+  {
+    id: 7,
+    category: "Breakfast 3",
+    desc: "ghjgjgjgjgjugjgujgsjg 3ereresea",
+    price: 21,
+    img: ""
+  },
+
+  {
+    id: 8,
+    category: "Lunch 3",
+    desc: "ghjgjgjgjgjugjgujgsjg huhzuizhihbbjm uizuizh",
+    price: 15,
+    img: ""
+  },
+  {
+    id: 9,
+    category: "Dinner 3",
+    desc: "ghjgjgjgjgjugjgujgsjg huhzuizhihbbjm poüpü+",
+    price: 20,
+    img: ""
+  }
+];
