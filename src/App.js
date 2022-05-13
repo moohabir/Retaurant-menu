806f17e36b95a0172815b944ba35004cfd32bb84
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-13 17:35:54 +0200

    need further update in navbar Link to be added onclick change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Menu from "./Menu";
+import Navbar from "./Navbar";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Restaurant menu app</h1>
+      <Navbar />
       <Menu />
     </div>
   );
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import data from "./data";
 export default function Menu() {
+  //const [btn, setBtn] = useState("All");
   return (
     <div className="menu">
-      <h1> Menu component</h1>
+      {data.map((item) => {
+        return (
+          <div key={item.id} className="items">
+            <img src={item.img} alt="" />
+            <h3>{item.category}</h3>
+            <p>{item.desc}</p>
+            <span>${item.price}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
